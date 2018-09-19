1a1b8d68833cfe3648f342667ce654a1de24973e
Henrique Säuberli <h.s@uberli.ch>
2018-09-19 17:02:38 +0200

    merger

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,16 +1,14 @@
-from server import app
+from flask import Flask
 from flask import render_template
+from server import app
 
-@app.route('/')
-def hello_world():
-    return app.send_static_file('index.html')
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        title='Table of Disruptive Technologies'
+    )
 
-@app.errorhandler(404)
-@app.route("/error404")
-def page_not_found(error):
-    return app.send_static_file('404.html')
-
-@app.errorhandler(500)
-@app.route("/error500")
-def requests_error(error):
-    return app.send_static_file('500.html')
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
