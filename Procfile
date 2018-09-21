623376f4396cb130f7f55cc128e673664ea75fdc
Henrique Säuberli <h.s@uberli.ch>
2018-09-21 20:49:00 +0200

    fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python disruptionApp/webapp.py
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from setuptools import setup
+"""
+Hello World app for running Python apps on Bluemix
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='server',
-    packages=['server'],
-    include_package_data=True,
-    install_requires=[
-        "flask"
-    ]
+    name='disrupting-technologies-table',
+    version='1.0.0',
+    description='Table of disrupting technologies',
+    long_description=long_description,
+    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
+    license='Apache-2.0'
 )
