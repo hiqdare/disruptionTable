f2a1dbf9ab4c33bd2baa42b29f10e3c938cc2782
Henrique Säuberli <h.s@uberli.ch>
2018-09-22 14:30:42 +0200

    next try

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, abort, session, request, redirect
 from flask.json import jsonify
 
-app = Flask(__name__, template_folder="../../public", static_folder="../../public", static_url_path='')
+app = Flask(__name__, template_folder="../public", static_folder="../public", static_url_path='')
 
 from server.routes import *
 from server.services import *
